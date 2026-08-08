98d2af0aa04734d98af69fd73b1469ae47fc1bd3
salmaelmaghraby <142322128+salmaelmaghraby@users.noreply.github.com>
2026-08-08 21:24:57 +0300

    Add files via upload

---
+++ task2.dart
@@ -0,0 +1,18 @@
+
+int x =0;
+int z = 0;
+int y = 0 ;
+
+  List<int> num = [x, y, z];
+  int sumNumbers(int x ,int y ,int z  ){
+
+    int sum = x+y+z ;
+    return sum;
+  }
+
+
+void main(){
+  //Write a function that takes a List<int> and returns the largest number.
+
+  print("Sum of numbers is : ${sumNumbers(60,50,20)}");
+}
