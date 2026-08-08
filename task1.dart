38e6d503fafb4e55ba62b92224d1651c899c9d39
salmaelmaghraby <142322128+salmaelmaghraby@users.noreply.github.com>
2026-08-08 18:35:16 +0300

    Add files via upload

---
+++ task1.dart
@@ -0,0 +1,14 @@
+
+int x= 0;
+int y = 0 ;
+int z = 0 ;
+
+List<int> num =  [x,y,z];
+void add(int x,int y,int z){
+  int sum = x+y+z ;
+  print("The sum of numbers is : $sum");
+}
+
+void main() {
+  add(6,7,8);
+}
